a186967d54c23e6e5d368a63de373182d3e79c53
MileeWe <73303670+MileeWe@users.noreply.github.com>
2020-12-11 22:18:53 +0300

    Normal jump
    Normal jump player

---
+++ FlappyBirdi/Assets/Camera/camMove.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+ 
+public class camMove : MonoBehaviour
+{
+    public GameObject Player;
+    void LateUpdate()
+    {
+        Vector3 p = Player.transform.position;
+        p.y = transform.position.y;
+        p.z = transform.position.z;
+        transform.position = p;
+    }
+}
---
+++ FlappyBirdi/Assets/player/playerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform tr;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+        double vel = rb.velocity.y;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * (10 - rb.velocity.y), ForceMode.Impulse);
+        }
+    }
+}
